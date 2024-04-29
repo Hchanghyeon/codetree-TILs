5a7f35b2061a4666bb721087d3415506f89df407
황창현 <changhyeon.h@kakao.com>
2024-04-29 23:23:56 +0900

    Title: 반복 출력하기, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240429/반복 출력하기/repeated-output.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final int count = Integer.parseInt(bufferedReader.readLine());
+
+        for(int i = 0; i < count; i++){
+            System.out.println("12345^&*()_");
+        }
+    }
+}
