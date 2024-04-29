77e59fbbe1955d5517c3dea4ed87212081460f6b
황창현 <changhyeon.h@kakao.com>
2024-04-29 22:41:34 +0900

    Title: 행복한 수열의 개수, Time: 286ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240429/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final String[] line = bufferedReader.readLine().split(" ");
+
+        final int count = Integer.parseInt(line[0]);
+        final int result = Integer.parseInt(line[1]);
+        final int[][] map = new int[count][count];
+
+        for(int i = 0; i < count; i++){
+            map[i] = Stream.of(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        if(result == 1){
+            System.out.println(count * 2);
+            return;
+        }
+
+        int resultNumber = 0;
+
+        for(int i = 0; i < count; i++){
+            int temp = 0;
+            int sum = 0;
+
+            for(int j = 0; j < count; j++){
+                int current = map[i][j]; // 현재 값
+
+                if(temp == current){ // 이전 값과 현재 값이 동일할 때
+                    sum++; // 값 올리기
+                } else {
+                    sum = 0; // 값 초기화
+                }
+
+                temp = current;
+            }
+
+            if(sum + 1 >= result){
+                resultNumber++;
+            }
+        }
+
+        for(int i = 0; i < count; i++){
+            int temp = 0;
+            int sum = 0;
+
+            for(int j = 0; j < count; j++){
+                int current = map[j][i]; // 현재 값
+
+                if(temp == current){ // 이전 값과 현재 값이 동일할 때
+                    sum++; // 값 올리기
+                } else {
+                    sum = 0; // 값 초기화
+                }
+
+                temp = current;
+            }
+
+            if(sum + 1 >= result){
+                resultNumber++;
+            }
+        }
+
+        System.out.println(resultNumber);
+    }
+}
