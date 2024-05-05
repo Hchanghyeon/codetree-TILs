4a0e9ccf70d1fc538bb261cddc99a71615d8819c
황창현 <changhyeon.h@kakao.com>
2024-05-05 22:33:50 +0900

    Title: 최소 경로로 탈출 하기, Time: 302ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240505/최소 경로로 탈출 하기/escape-with-min-distance.java
+++ 240505/최소 경로로 탈출 하기/escape-with-min-distance.java
@@ -25,6 +25,13 @@ public class Main {
 
         search(0, 0);
 
+        int result = map[N-1][M-1];
+
+        if(result <= 1){
+            System.out.println(-1);
+            return;
+        }
+
         System.out.println(map[N-1][M-1] - 1);
     }
 
