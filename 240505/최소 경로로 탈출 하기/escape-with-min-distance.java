b9f50e5d435e7f1d025afb0364ce059a3d9cd93f
황창현 <changhyeon.h@kakao.com>
2024-05-05 22:32:39 +0900

    Title: 최소 경로로 탈출 하기, Time: 287ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240505/최소 경로로 탈출 하기/escape-with-min-distance.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+import java.util.*;
+
+public class Main {
+
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static final int[] dx = {0, 0, -1, 1};
+    private static final int[] dy = {-1, 1, 0, 0};
+    
+    private static int[][] map;
+    private static int N, M;
+
+    public static void main(String[] args) throws Exception{
+        final String[] line = bufferedReader.readLine().split(" ");
+        N = Integer.parseInt(line[0]);
+        M = Integer.parseInt(line[1]);
+
+        map = new int[N][M];
+
+        for(int i = 0; i < N; i++){
+            map[i] = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        search(0, 0);
+
+        System.out.println(map[N-1][M-1] - 1);
+    }
+
+    private static void search(int x, int y){
+        final Deque<int[]> deque = new ArrayDeque<>();
+        int[] point = new int[]{x, y};
+        deque.add(point);
+
+        while(!deque.isEmpty()){
+            int[] currentPoint = deque.pollLast();
+            
+            for(int i = 0; i < 4; i++){
+                int nextX = dx[i] + currentPoint[0];
+                int nextY = dy[i] + currentPoint[1];
+
+                if(!isBoundary(nextX, nextY)){
+                    continue;
+                }
+                
+                map[nextY][nextX] = map[currentPoint[1]][currentPoint[0]] + 1;
+                deque.add(new int[]{nextX, nextY});
+            }
+        }
+
+    }
+
+    private static boolean isBoundary(int x, int y){
+        return x >= 0 && x < M && y >= 0 && y < N && map[y][x] == 1;
+    }
+}
