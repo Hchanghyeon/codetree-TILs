b6397f5e7e6a03883463c3b1e32a6224d4f22fa3
황창현 <changhyeon.h@kakao.com>
2024-05-23 19:07:38 +0900

    Title: 피보나치 수, Time: 78ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240523/피보나치 수/fibonacci-number.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] dp = new int[46];
+        int number = Integer.parseInt(bufferedReader.readLine());
+
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i = 3; i < 45; i++){
+            dp[i] = dp[i - 1] + dp[i + 1];
+        }
+
+        System.out.println(dp[number]);
+    }
+}
