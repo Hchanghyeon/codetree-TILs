eadc3a80fce63ad9c757d476bfa003fdeb6ac923
황창현 <changhyeon.h@kakao.com>
2024-06-06 14:35:17 +0900

    Title: 아름다운 수, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240606/아름다운 수/beautiful-number.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class Main {
+
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static final List<Integer> numbers = new ArrayList<>();
+    private static int N = 0, result = 0;
+
+    public static void main(String[] args) throws Exception {
+        N = Integer.parseInt(bufferedReader.readLine());
+
+        recursive(0);
+        System.out.println(result);
+    }
+
+    private static void recursive(int length){
+        if(length == N){
+            if(findBeautifulNumber()){
+                result++;
+            }
+            return;
+        }
+
+        for(int i = 1; i <= 4; i++){
+            numbers.add(i);
+            recursive(length + 1);
+            numbers.remove(numbers.size() - 1);
+        }
+    }
+
+    private static boolean findBeautifulNumber(){
+        int prev = numbers.get(0);
+        int count = 1;
+
+        for(int i = 1; i < numbers.size(); i++){
+            if(prev == numbers.get(i)){
+                count++;
+            } else {
+                if(count != prev){
+                    return false;
+                }
+
+                count = 1;
+            }
+
+            prev = numbers.get(i);
+        }
+     
+        return count == numbers.get(numbers.size() - 1) || count % numbers.get(numbers.size() - 1) == 0;
+    }
+}
