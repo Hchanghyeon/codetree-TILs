8fa691be5682e1815a5ea3af9ebc0a5e2fb92459
황창현 <changhyeon.h@kakao.com>
2024-06-11 10:34:29 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240611/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static int N = 0;
+
+    public static void main(String[] args) throws Exception {
+        
+        N = Integer.parseInt(bufferedReader.readLine());
+        int result = recursive(1, 1, 2);
+        System.out.println(result);
+    }
+
+    private static int recursive(int num1, int num2, int count){
+        if(N == count){
+            return num2;
+        }
+
+        return recursive(num2, num1 + num2, count + 1);
+    }
+}
