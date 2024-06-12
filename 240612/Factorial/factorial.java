e5ab0ad2b5572c4cc9e169a03217d297132a8775
황창현 <changhyeon.h@kakao.com>
2024-06-12 22:46:35 +0900

    Title: Factorial, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240612/Factorial/factorial.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static int N = 0;
+
+    public static void main(String[] args) throws Exception {
+        
+        N = Integer.pasreInt(bufferedReader.readLine());
+
+        System.out.println(recursive(N));
+    }
+
+    private static int recursive(int number){
+        if(number == 0){
+            return 1;
+        }
+        
+        return recursive(number - 1) * number;
+    }
+}
