15d7b76401ffbf0cbf47985150d5d9d4d0accacd
황창현 <changhyeon.h@kakao.com>
2024-06-12 22:54:03 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 108ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240612/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static int N = 0;
+
+    public static void main(String[] args) throws Exception {
+        
+        N = Integer.parseInt(bufferedReader.readLine());
+        
+        System.out.println(recursive(N));
+    }
+
+    private static int recursive(int number){
+        if(number == 1){
+            return 1;
+        }
+
+        if(number == 2){
+            return 2;
+        }
+
+        return recursive(number - 2) + number;
+    }
+}
