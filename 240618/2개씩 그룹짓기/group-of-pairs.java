c2f2edc3a9ba82c6c9e9076880c7bf8ebbc6cd8a
황창현 <changhyeon.h@kakao.com>
2024-06-18 08:30:23 +0900

    Title: 2개씩 그룹짓기, Time: 283ms, Memory: 14MB, Status: Passed - CodeTree

--- 240618/2개씩 그룹짓기/group-of-pairs.java
+++ 240618/2개씩 그룹짓기/group-of-pairs.java
@@ -6,28 +6,16 @@ public class Main {
 
     private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     private static int N = 0, sum = 0;
-    private static int[] numbers;
+    private static Integer[] numbers;
 
     public static void main(String[] args) throws Exception {
         N = Integer.parseInt(bufferedReader.readLine());
-        numbers = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        numbers = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
 
         Arrays.sort(numbers);
-
-        int count = 1;
-        int prev = numbers[numbers.length - 1];
-        sum += prev;
-
-        for(int i = numbers.length - 2; i >= 0; i--){
-            if(count == 2){
-                break;
-            }
-
-            if(prev != numbers[i]){
-                sum += numbers[i]; 
-                count++;
-                prev = numbers[i];
-            }
+        
+        for(int i = 0; i < numbers.length / 2; i++){
+            sum = Math.max(sum, numbers[i] + numbers[numbers.length - 1 - i]);
         }
 
         System.out.println(sum);
