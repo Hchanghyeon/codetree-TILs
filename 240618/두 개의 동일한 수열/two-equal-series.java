40fdec8110d69b3d806cba0a1d74a957312785f0
황창현 <changhyeon.h@kakao.com>
2024-06-18 07:52:28 +0900

    Title: 두 개의 동일한 수열, Time: 277ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240618/두 개의 동일한 수열/two-equal-series.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static int N = 0;
+    private static int[] A;
+    private static int[] B;
+
+    public static void main(String[] args) throws Exception {
+        N = Integer.parseInt(bufferedReader.readLine());
+        A = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        B = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+ 
+        for(int i = 0; i < N; i++){
+            if(A[i] != B[i]){
+                System.out.println("No");
+                break;
+            }
+        }
+
+        System.out.println("Yes");
+    }
+}
