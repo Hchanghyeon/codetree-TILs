11673d83fd74ff74a2cae54b4f2d6957024df23c
황창현 <changhyeon.h@kakao.com>
2024-06-18 09:02:32 +0900

    Title: 중앙값 계산 2, Time: 271ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240618/중앙값 계산 2/get-median-2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static int N;
+    private static int[] numbers;
+    private static int[] result;
+
+    public static void main(String[] args) throws Exception {
+        N = Integer.parseInt(bufferedReader.readLine());
+        numbers = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        result = new int[N];
+
+        for(int i = 0; i < N; i++){
+            result[i] = numbers[i];
+
+            if((i + 1) % 2 != 0){
+                Arrays.sort(result, 0, i);
+                System.out.print(result[i / 2] + " ");
+            }
+        }
+    }
+}
