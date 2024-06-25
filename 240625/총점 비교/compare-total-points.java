f115594f7662bd6bb9d39fa0e71e2c2646827b4b
황창현 <changhyeon.h@kakao.com>
2024-06-25 17:03:22 +0900

    Title: 총점 비교, Time: 271ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240625/총점 비교/compare-total-points.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static Student[] students;
+    private static int N;
+
+    public static void main(String[] args) throws Exception {
+        N = Integer.parseInt(bufferedReader.readLine());
+        students = new Student[N];
+
+        for(int i = 0; i < N; i++){
+            String[] line = bufferedReader.readLine().split(" ");
+            students[i] = new Student(line[0], Integer.parseInt(line[1]), Integer.parseInt(line[2]), Integer.parseInt(line[3]));
+        }
+
+        Arrays.sort(students, (a, b) -> (a.korean + a.english + a.math) - (b.korean + b.english + b.math));
+
+        for(Student student : students){
+            System.out.println(student.toString());
+        }
+    }
+
+    static class Student{
+        private String name;
+        private int korean;
+        private int english;
+        private int math;
+
+        public Student(String name, int korean, int english, int math){
+            this.name = name;
+            this.korean = korean;
+            this.english = english;
+            this.math = math;
+        }
+
+        public String toString(){
+            return name + " " + korean + " " + english + " " + math;
+        }
+    }
+}
